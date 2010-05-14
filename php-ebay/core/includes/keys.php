d84ad49013fe6b63b4cb80193eb1deaa7254de9e
Andy Nu <anu@gilt.com>
2010-05-13 23:08:54 -0700

    oops...

--- php-ebay/core/includes/keys.php
+++ php-ebay/core/includes/keys.php
@@ -13,18 +13,18 @@
     fclose($handle);
 
     if ($production) {
-        $devID = 'U7P65SNTQNFT9EJGN89921L966CKDV';   // these prod keys are different from sandbox keys
-        $appID = 'AndyNu8d7-9b52-4508-b820-b077840ac1a';
-        $certID = '0cc07fab-50da-4327-bece-f373bd480b91';
+        $devID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';   // these prod keys are different from sandbox keys
+        $appID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+        $certID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
         //set the Server to use (Sandbox or Production)
         $serverUrl = 'https://api.ebay.com/ws/api.dll';      // server URL different for prod and sandbox
         //the token representing the eBay user to assign the call with
         $userToken = $contents;
     } else {  
         // sandbox (test) environment
-        $devID = 'U7P65SNTQNFT9EJGN89921L966CKDV';         // insert your devID for sandbox
-        $appID = 'ANDYNUK1TS9NI4YI862DM76P19HDM3';   // different from prod keys
-        $certID = 'O71X14MKCFS$P9G5LIY48-675F23YB';  // need three 'keys' and one token
+        $devID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';         // insert your devID for sandbox
+        $appID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';   // different from prod keys
+        $certID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';  // need three 'keys' and one token
         //set the Server to use (Sandbox or Production)
         $serverUrl = 'https://api.sandbox.ebay.com/ws/api.dll';
         // the token representing the eBay user to assign the call with
