2ace202d3d6d9261b9a3691075059425e5a3964b
Andy Nu <anu@gilt.com>
2010-05-18 11:09:22 -0700

    removed the Xs

--- php-ebay/core/includes/keys.php
+++ php-ebay/core/includes/keys.php
@@ -13,24 +13,22 @@
     fclose($handle);
 
     if ($production) {
-        $devID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';   // these prod keys are different from sandbox keys
-        $appID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-        $certID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+        $devID = 'YOUR EBAY PRODUCTION DEV KEY';   // these prod keys are different from sandbox keys
+        $appID = 'YOUR EBAY PRODUCTION APP KEY';
+        $certID = 'YOUR EBAY PRODUCTION CERT KEY';
         //set the Server to use (Sandbox or Production)
         $serverUrl = 'https://api.ebay.com/ws/api.dll';      // server URL different for prod and sandbox
         //the token representing the eBay user to assign the call with
         $userToken = $contents;
     } else {  
         // sandbox (test) environment
-        $devID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';         // insert your devID for sandbox
-        $appID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';   // different from prod keys
-        $certID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';  // need three 'keys' and one token
+        $devID = 'YOUR EBAY SANDBOX DEV KEY';         // insert your devID for sandbox
+        $appID = 'YOUR EBAY SANDBOX APP KEY';   // different from prod keys
+        $certID = 'YOUR EBAY SANDBOX CERT KEY';  // need three 'keys' and one token
         //set the Server to use (Sandbox or Production)
         $serverUrl = 'https://api.sandbox.ebay.com/ws/api.dll';
         // the token representing the eBay user to assign the call with
         // this token is a long string - don't insert new lines - different from prod token
         $userToken = $contents;                 
     }
-    
-    
 ?>
